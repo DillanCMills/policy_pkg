aa7589347b4c1821b8359d7be5d469f12fde0827
Dillan Mills <github-svls@dillan.org>
2023-11-09 23:11:36 -0700

    work on v3 examples

--- dvcon_2024/example_code/v3_extended_policies/addr_txn.svh
+++ dvcon_2024/example_code/v3_extended_policies/addr_txn.svh
@@ -1,56 +1,37 @@
 class addr_l1_txn;
-  rand bit [31:0] addr;
-  rand int        size;
-  rand policy_base#(addr_l1_txn) policy_l1[$];
+    rand bit [31:0]   addr;
+    rand int          size;
+    rand policy_queue policy;
 
-  constraint c_size {size inside {1, 2, 4};}
+    constraint c_size {size inside {1, 2, 4};}
 
-  function void pre_randomize;
-    foreach(policy_l1[i]) policy_l1[i].set_item(this);
-  endfunction
+    function void pre_randomize;
+        foreach(policy[i]) policy[i].set_item(this);
+    endfunction
 endclass
 
 
 class addr_l2_txn extends addr_l1_txn;
-  rand int f2;
+    rand int f2;
 endclass
 
 
-class addr_l3_txn extends addr_l2_txn;
-  rand int f3;
-endclass
+class addr_constrained_txn extends addr_l2_txn;
+    function new;
+        policy_queue pcy = new;
 
+        addr_permit_policy::base_policy   permit   = addr_permit_policy::PERMIT_POLICY();
+        addr_prohibit_policy::base_policy prohibit = addr_prohibit_policy::PROHIBIT_POLICY();
 
-class addr_l4_txn extends addr_l3_txn;
-  rand int f4;
-endclass
+        permit.add('h00000000, 'h0000FFFF);
+        permit.add('h10000000, 'h1FFFFFFF);
+        pcy.add(permit);
 
+        prohibit.add('h13000000, 'h130FFFFF);
+        pcy.add(prohibit);
+        
+        pcy.add(addr_l2_policy::FIXED_F2('h12345678));
 
-class addr_constrained_txn extends addr_l4_txn;
-  function new;
-    addr_permit_policy        permit = new;
-    addr_prohibit_policy      prohibit = new;
-    addr_l2_policy            l2 = new;
-    addr_l3_policy            l3 = new;
-    addr_l4_policy            l4 = new;
-    policy_list#(addr_l1_txn) pcy_l1 = new;
-
-    permit.add('h00000000, 'h0000FFFF);
-    permit.add('h10000000, 'h1FFFFFFF);
-    pcy_l1.add(permit);
-
-    prohibit.add('h13000000, 'h130FFFFF);
-    pcy_l1.add(prohibit);
-    
-    l2.set('h12345678);
-    pcy_l1.add(l2);
-    
-    l3.set('h55555555);
-    pcy_l1.add(l3);
-    
-    l4.set('h87654321);
-    pcy_l1.add(l4);
-
-    this.policy_l1 = {pcy_l1};
-  endfunction
+        this.policy = {pcy};
+    endfunction
 endclass
