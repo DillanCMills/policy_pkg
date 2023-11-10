ce879312eb5a63f91fb552b43e70127a617837af
thechipabides <150401013+thechipabides@users.noreply.github.com>
2023-11-09 18:21:43 -0800

    Update policy_imp.svh
    Convert policy_imp to virtual class and tune default implementations:
    
    1. let name, description, and copy remain pure virtual; concrete implementations will all implement these.
    
    2. make policy's ITEM protected (and rand) so that only calls to set_item() can assign it.
    
    3. implement item_is_compatible() using an internal $cast()
    
    4. implement set_item with:
       a) error on NULL passed (this should not happen)
       b) warning on incompatible item passed; use local debug APIs
       c) info on working assignment
    
    5. turn off rand_mode() on failed assignments to prevent constraints from participating or complaining

--- policy_pkg/policy_imp.svh
+++ policy_pkg/policy_imp.svh
@@ -1,35 +1,52 @@
 `ifndef __POLICY_IMP__
 `define __POLICY_IMP__
 
-class policy_imp #(type ITEM=uvm_object) implements policy;
+virtual class policy_imp #(type ITEM=uvm_object) implements policy;
 
-    ITEM item;
+    protected rand ITEM m_item;
 
-    virtual function string name();
-        return ($typename(this));
-    endfunction: name
+    pure virtual function string name();
+    pure virtual function string description();
+    pure virtual function policy copy();
 
     virtual function string type_name();
+        return( ITEM::type_name );
     endfunction: type_name
 
-    virtual function string description();
-    endfunction: description
-
     virtual function bit item_is_compatible(uvm_object item);
+        ITEM local_item;
+
+        if (item == null) return( 0 );
+        else return( $cast(local_item, item) );
     endfunction: item_is_compatible
 
     virtual function void set_item(uvm_object item);
+        if (item == null) begin
+            `uvm_error("policy::set_item()", "NULL item passed")
 
-        if (item == null)
-            `uvm_fatal("policy::set_item()", "NULL item passed")
+        end else if ((this.item_is_compatible(item)) && $cast(this.m_item, item)) begin
+            `uvm_info(
+                "policy::set_item()",
+                $sformatf(
+                    "policy <%s> applied to item <%s>: %s",
+                    this.name(), item.get_name(), this.description
+                ),
+                UVM_FULL
+            )
+            this.m_item.rand_mode( 1 );
 
-        else if (!$cast(this.item, item))
-            `uvm_warning("policy::set_item()", $sformatf("Item type <%s> is not compatible with policy type <%s>", item.get_type_name(), $typename(this.item)))
-
+        end else begin
+            `uvm_warning(
+                "policy::set_item()",
+                $sformatf(
+                    "Item <%s> type <%s> is not compatible with policy <%s> type <%s>",
+                    item.get_name(), item.get_type_name(), this.name(), this.type_name()
+                )
+            )
+            this.m_item = null;
+            this.m_item.rand_mode( 0 );
+        end
     endfunction: set_item
-
-    virtual function policy copy();
-    endfunction: copy
 
 endclass: policy_imp
 
