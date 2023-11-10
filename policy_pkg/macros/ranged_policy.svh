16313834c1a8a29fcecb5d50e37477e4abe3e8f8
thechipabides <150401013+thechipabides@users.noreply.github.com>
2023-11-09 18:49:46 -0800

    Update ranged_policy.svh

--- policy_pkg/macros/ranged_policy.svh
+++ policy_pkg/macros/ranged_policy.svh
@@ -8,7 +8,7 @@
 
 // Policy class definition
 `define RANGED_POLICY_CLASS(POLICY, TYPE, field, RADIX="%0p")                 \
-    class POLICY``_policy extends LOCAL_POLICY_T                              \
+    class POLICY``_policy extends base_policy                                 \
         typedef TYPE        l_field_t;                                        \
                                                                               \
         protected TYPE      m_low;                                            \
