2ed16984bd3f2c42ad742bc3282fd691c242f193
Dillan Mills <github-svls@dillan.org>
2023-11-09 23:52:55 -0700

    static function return type not valid

--- dvcon_2024/example_code/v4_embedded_policies/testbench.sv
+++ dvcon_2024/example_code/v4_embedded_policies/testbench.sv
@@ -1,6 +1,5 @@
 `include "addr_types.svh"
 `include "policy_base.svh"
-`include "addr_policies.svh"
 `include "addr_txn.svh"
 
       
@@ -10,7 +9,5 @@ module tb;
     txn.randomize();
     $display("addr=%0h", txn.addr);
     $display("f2=%0h", txn.f2);
-    $display("f3=%0h", txn.f3);
-    $display("f4=%0h", txn.f4);
   end
 endmodule
