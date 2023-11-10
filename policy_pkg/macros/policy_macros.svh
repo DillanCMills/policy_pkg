74997236df4b6b2f327943fcd83211493b606eab
thechipabides <150401013+thechipabides@users.noreply.github.com>
2023-11-09 18:47:45 -0800

    Update policy_macros.svh
    Tweak base_policy definition:
    
    The class name "base_policy" is much more readable and intuitive
    The name `m_base_policy is consistent with UVM macro names

--- policy_pkg/macros/policy_macros.svh
+++ policy_pkg/macros/policy_macros.svh
@@ -7,17 +7,17 @@
 
 `define start_policies(cls)                     \
 class POLICIES;                                 \
-`LOCAL_POLICY_IMP(cls)
+`m_base_policy(cls)
 
 `define start_extended_policies(cls, parent)    \
 class POLICIES extends parent::POLICIES;        \
-`LOCAL_POLICY_IMP(cls)
+`m_base_policy(cls)
 
 `define end_policies                            \
 endclass: POLICIES
 
-`define LOCAL_POLICY_IMP(cls)                   \
-    typedef policy_imp#(cls) LOCAL_POLICY_T;
+`define m_base_policy(cls)                      \
+    typedef policy_imp#(cls) base_policy;
 
 
 //================================================
