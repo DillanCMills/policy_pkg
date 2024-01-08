8c46c828dc1e7690b251a99adb44e757d0e334ed
Dillan Mills <github-svls@dillan.org>
2024-01-08 14:29:48 -0700

    update example policy macro, fix latex file name changes

--- dvcon_2024/example_code/v5_policy_macros/policy_macros.svh
+++ dvcon_2024/example_code/v5_policy_macros/policy_macros.svh
@@ -1,22 +1,22 @@
 // Fixed-value policy class and constructor macro
-`define fixed_policy(POLICY, TYPE, field)                                     \
-`m_fixed_policy_class(POLICY, TYPE, field)                                    \
-`m_fixed_policy_constructor(POLICY, TYPE, field)
+`define fixed_policy(POLICY, FIELD, TYPE)                                     \
+`m_fixed_policy_class(POLICY, FIELD, TYPE)                                    \
+`m_fixed_policy_constructor(POLICY, TYPE)
 
-`define m_fixed_policy_class(POLICY, TYPE, field)                             \
+`define m_fixed_policy_class(POLICY, FIELD, TYPE)                             \
     class POLICY``_policy extends base_policy;                                \
-        protected TYPE m_fixed_value;                                         \
+        protected TYPE l_val;                                                 \
                                                                               \
         constraint c_fixed_value {                                            \
-            m_item != null -> m_item.field == m_fixed_value;                  \
+            (m_item != null) -> (m_item.FIELD == TYPE'(l_val));               \
         }                                                                     \
                                                                               \
         function new(TYPE value);                                             \
-            this.m_fixed_value = value;                                       \
+            this.l_val = value;                                               \
         endfunction                                                           \
     endclass: POLICY``_policy
 
-`define m_fixed_policy_constructor(POLICY, TYPE, field)                       \
+`define m_fixed_policy_constructor(POLICY, TYPE)                              \
     static function POLICY``_policy POLICY(TYPE value);                       \
         POLICY = new(value);                                                  \
     endfunction: POLICY
